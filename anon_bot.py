8ef88b2e57ad3ab1a8ac3fd0c553b8b788d9406b
Prox <39493761+The-Proxster@users.noreply.github.com>
2018-05-21 16:21:31 -0400

    Update anon_bot.py

--- anon_bot.py
+++ anon_bot.py
@@ -7,40 +7,11 @@ from random import randint
 import time
 import datetime
 
-startup_extensions = []
-
 bot = commands.Bot(command_prefix='?A ')
 @bot.event
 async def on_ready():
     print ("Anon Bot online.")
     await bot.change_presence(game=discord.Game(name='anonymously.')) #Makes it so it says bot is 'playing anonymously.'
-
-def raw_input(x):
-    input(x)
-
-@bot.command()
-async def load(extension_name : str):
-    """Loads an extension."""
-    try:
-        bot.load_extension(extension_name)
-    except (AttributeError, ImportError) as e:
-        await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
-        return
-    await bot.say("{} loaded.".format(extension_name))
-
-@bot.command()
-async def unload(extension_name : str):
-    """Unloads an extension."""
-    bot.unload_extension(extension_name)
-    await bot.say("{} unloaded.".format(extension_name))
-
-if __name__ == "__main__":
-    for extension in startup_extensions:
-        try:
-            bot.load_extension(extension)
-        except Exception as e:
-            exc = '{}: {}'.format(type(e).__name__, e)
-            print('Failed to load extension {}\n{}'.format(extension, exc))
 
 @bot.command(pass_context=True)
 async def info(ctx, user: discord.Member):
